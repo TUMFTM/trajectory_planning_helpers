8ae74621469a8ad185a0b850982509dc3ba3fac0
Alexander Heilmeier <heilmeier@ftm.mw.tum.de>
2019-06-07 13:03:25 +0200

    Fixed ax_profile bug v0.22

--- trajectory_planning_helpers/calc_ax_profile.py
+++ trajectory_planning_helpers/calc_ax_profile.py
@@ -13,9 +13,9 @@ def calc_ax_profile(vx_profile: np.ndarray, el_lengths: np.ndarray) -> np.ndarra
     vx_profile: array containing the velocity profile used as a basis for the acceleration calculations.
     el_lengths: array containing the element lengths between every point of the velocity profile.
 
-    len(vx_profile) = len(ax_profile) + 1 = len(el_lengths) + 1
+    len(vx_profile) = len(ax_profile) = len(el_lengths) + 1
 
-    Assumes zero acceleration for the last point of the profile!
+    Assumes zero acceleration for the last point of the acceleration profile.
     """
 
     # check inputs
@@ -23,7 +23,8 @@ def calc_ax_profile(vx_profile: np.ndarray, el_lengths: np.ndarray) -> np.ndarra
         raise ValueError("Array size of vx_profile should be 1 element bigger than el_lengths!")
 
     # calculate longitudinal acceleration profile array numerically: (v_end^2 - v_beg^2) / 2*s
-    ax_profile = (np.power(vx_profile[1:], 2) - np.power(vx_profile[:-1], 2)) / (2 * el_lengths)
+    ax_profile = np.zeros(vx_profile.size)
+    ax_profile[:-1] = (np.power(vx_profile[1:], 2) - np.power(vx_profile[:-1], 2)) / (2 * el_lengths)
 
     return ax_profile
 
