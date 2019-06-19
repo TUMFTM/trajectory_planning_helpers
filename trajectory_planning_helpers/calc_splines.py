1214e46d0d955d490fce474f86cf5f37e0313b07
Alexander Heilmeier <heilmeier@ftm.mw.tum.de>
2019-06-19 10:26:38 +0200

    Fixed bug in calc_splines v0.29

--- trajectory_planning_helpers/calc_splines.py
+++ trajectory_planning_helpers/calc_splines.py
@@ -54,6 +54,10 @@ def calc_splines(path: np.ndarray,
     if use_dist_scaling and el_lengths is None:
         el_lengths = np.sqrt(np.sum(np.power(np.diff(path, axis=0), 2), axis=1))
 
+    # if closed and use_dist_scaling active append element length in order to obtain overlapping scaling
+    if use_dist_scaling and closed:
+        el_lengths = np.hstack((el_lengths, el_lengths[0]))
+
     # get number of splines
     no_splines = path.shape[0] - 1
 
