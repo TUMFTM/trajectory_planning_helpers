80682324fa809299a34897c7e59892cb8185cd01
Alexander Heilmeier <heilmeier@ftm.mw.tum.de>
2019-06-24 14:45:10 +0200

    Added option to jump over curvature calculation in calc_head_curv_an (no new version)

--- trajectory_planning_helpers/calc_head_curv_an.py
+++ trajectory_planning_helpers/calc_head_curv_an.py
@@ -5,7 +5,8 @@ import trajectory_planning_helpers.normalize_psi
 def calc_head_curv_an(coeffs_x: np.ndarray,
                       coeffs_y: np.ndarray,
                       ind_spls: np.ndarray,
-                      t_spls: np.ndarray) -> tuple:
+                      t_spls: np.ndarray,
+                      calc_curv: bool = True) -> tuple:
     """
     Created by:
     Alexander Heilmeier
@@ -19,6 +20,7 @@ def calc_head_curv_an(coeffs_x: np.ndarray,
     coeffs_y:   coefficient matrix of the y splines with size (no_splines x 4).
     ind_spls:   contains the indices of the splines that hold the points for which we want to calculate heading/curv.
     t_spls:     containts the relative spline coordinate values (t) of every point on the splines.
+    calc_curv:  bool flag to show if curvature should be calculated as well (kappa is set 0.0 otherwise).
 
     Outputs:
     psi:        heading at every point.
@@ -34,27 +36,40 @@ def calc_head_curv_an(coeffs_x: np.ndarray,
     if ind_spls.size != t_spls.size:
         raise ValueError("ind_spls and t_spls must have the same length!")
 
+    # ------------------------------------------------------------------------------------------------------------------
+    # CALCULATE HEADING ------------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
+
     # calculate required derivatives
     x_d = coeffs_x[ind_spls, 1] \
           + 2 * coeffs_x[ind_spls, 2] * t_spls \
           + 3 * coeffs_x[ind_spls, 3] * np.power(t_spls, 2)
 
-    x_dd = 2 * coeffs_x[ind_spls, 2] \
-           + 6 * coeffs_x[ind_spls, 3] * t_spls
-
     y_d = coeffs_y[ind_spls, 1] \
           + 2 * coeffs_y[ind_spls, 2] * t_spls \
           + 3 * coeffs_y[ind_spls, 3] * np.power(t_spls, 2)
-
-    y_dd = 2 * coeffs_y[ind_spls, 2] \
-           + 6 * coeffs_y[ind_spls, 3] * t_spls
 
     # calculate heading psi (pi/2 must be substracted due to our convention that psi = 0 is north)
     psi = np.arctan2(y_d, x_d) - np.pi / 2
     psi = trajectory_planning_helpers.normalize_psi.normalize_psi(psi)
 
-    # calculate curvature kappa
-    kappa = (x_d * y_dd - y_d * x_dd) / np.power(np.power(x_d, 2) + np.power(y_d, 2), 1.5)
+    # ------------------------------------------------------------------------------------------------------------------
+    # CALCULATE CURVATURE ----------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
+
+    if calc_curv:
+        # calculate required derivatives
+        x_dd = 2 * coeffs_x[ind_spls, 2] \
+               + 6 * coeffs_x[ind_spls, 3] * t_spls
+
+        y_dd = 2 * coeffs_y[ind_spls, 2] \
+               + 6 * coeffs_y[ind_spls, 3] * t_spls
+
+        # calculate curvature kappa
+        kappa = (x_d * y_dd - y_d * x_dd) / np.power(np.power(x_d, 2) + np.power(y_d, 2), 1.5)
+
+    else:
+        kappa = 0.0
 
     return psi, kappa
 
