Fixed bug in spline calculation v0.70

trajectory_planning_helpers/calc_splines.py
@@ -73,11 +73,13 @@ def calc_splines(path: np.ndarray,
     # if distances between path coordinates are not provided but required, calculate euclidean distances as el_lengths
     if use_dist_scaling and el_lengths is None:
         el_lengths = np.sqrt(np.sum(np.power(np.diff(path, axis=0), 2), axis=1))
+    elif el_lengths is not None:
+        el_lengths = np.copy(el_lengths)
 
     # if closed and use_dist_scaling active append element length in order to obtain overlapping elements for proper
     # scaling of the last element afterwards
     if use_dist_scaling and closed:
-        el_lengths = np.append((el_lengths, el_lengths[0]))
+        el_lengths = np.append(el_lengths, el_lengths[0])
 
     # get number of splines
     no_splines = path.shape[0] - 1